0212e898089749ec58950c7fb06f4a09a4a76be6
Odwori1 <odwori.okuku@gmail.com>
2024-04-21 19:43:03 -0700

    7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """The states list page."""
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    return render_template('7-states_list.html', states=all_states)
+
+
+@app.teardown_appcontext
+def flask_teardown(exc):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
